f48be470ccc50493af927dcdbb8768ba76b1872c
Matheus Antonino <matrajano12@gmail.com>
2020-04-27 20:13:38 -0300

    Create project

---
+++ android/app/src/main/kotlin/br/com/antonino/search_gif_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.antonino.search_gif_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:search_gif_flutter/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Search Gif`s',
+    home: HomePage(),
+  ));
+}
